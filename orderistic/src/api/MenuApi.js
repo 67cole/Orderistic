4bd426973553b36f269021e9ee722393258b6947
JonathanQiu <jonathan.qiu2001@outlook.com>
2023-06-24 02:04:11 +1000

    Finished all assigned tickets of Sprint 1 (Himmy-19,20,21,22)

--- orderistic/src/api/MenuApi.js
+++ orderistic/src/api/MenuApi.js
@@ -6,10 +6,12 @@ import {
   getDocs,
   onSnapshot,
   addDoc,
+  deleteDoc,
 } from "firebase/firestore";
 
-export async function ViewItems() {
-  const q = collection(db, "staff");
+//Allows all users to view the menu HIMMY-19
+export async function ViewMenu() {
+  const q = collection(db, "menu");
   console.log("Grabbing Items");
   const querySnapshot = await getDocs(q);
   querySnapshot.forEach((doc) => {
@@ -18,8 +20,21 @@ export async function ViewItems() {
   });
 }
 
-export async function AddItem(email) {
-  await addDoc(collection(db, "staff"), {
-    email: email,
-  });
+//Allows staff to add items to the menu HIMMY-20
+export async function AddItem(item) {
+  const res = await addDoc(collection(db, 'menu'), item);
+  console.log('Added document with ID: ', res.id);
 }
+
+//Allows staff to remove items from the menu HIMMY-22
+export async function RemoveItem(id) {
+  const res = await deleteDoc(doc(db, 'menu', id));
+  console.log('Removed document with ID: ', id);
+}
+
+//Allows staff to update items on the menu HIMMY-21
+export async function UpdateItem(id, item) {
+  const res = await setDoc(doc(db, 'menu', id), item);
+  console.log('Updated document with ID: ', id);
+}
+
