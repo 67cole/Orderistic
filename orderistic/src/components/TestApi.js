e06393a1df474b1709fdbf9ce9b5aeed7f1a5a75
Colin <speedtouch1733@gmail.com>
2023-06-26 03:13:49 +1000

    added test cases for new order functions (HIMMY-19)

--- orderistic/src/components/TestApi.js
+++ orderistic/src/components/TestApi.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import { Card, Button, Alert } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
-import { AddItem, AddItems, ViewMenu, RemoveItem, UpdateItem, returnData } from "../api/MenuApi";
+import { AddItem, AddItems, ViewMenu, RemoveItem, UpdateItem, returnFoodData } from "../api/MenuApi";
+import { returnOrderData, viewOrder, addOrder, removeOrder } from "../api/OrderApi"
 
 export default function TestApi() {
   const [error, setError] = useState("");
@@ -14,8 +15,8 @@ export default function TestApi() {
     ViewMenu();
   }
 
-  async function handleData() {
-    returnData();
+  async function handleFoodData() {
+    returnFoodData();
   }
 
   async function handleAdd() {
@@ -41,6 +42,15 @@ export default function TestApi() {
     rating: 5});
   }
 
+  // Order test functions
+  async function handleOrder() {
+    viewOrder();
+  }
+
+  async function handleOrderData() {
+    returnOrderData();
+  }
+
   return (
     <>
       <Card style={{ width: "75vw", maxWidth: "600px" }}>
@@ -50,8 +60,8 @@ export default function TestApi() {
           <Button className="btn btn-primary w-100 mt-3" onClick={handleMenu}>
             View Menu
           </Button>
-          <Button className="btn btn-primary w-100 mt-3" onClick={handleData}>
-            View Data
+          <Button className="btn btn-primary w-100 mt-3" onClick={handleFoodData}>
+            View Food Data
           </Button>
           <Button className="btn btn-primary w-100 mt-3" onClick={handleAdd}>
             Add Item
@@ -61,6 +71,12 @@ export default function TestApi() {
           </Button>
           <Button className="btn btn-primary w-100 mt-3" onClick={handleUpdate}>
             Update Item
+          </Button>
+          <Button className="btn btn-primary w-100 mt-3" onClick={handleOrder}>
+            View Order
+          </Button>
+          <Button className="btn btn-primary w-100 mt-3" onClick={handleOrderData}>
+            View Order Data
           </Button>
           <Link to="/staff-dashboard" className="btn btn-danger w-100 mt-3">
             Go Back
