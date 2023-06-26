38b5249908979905bbe76b993240aca1697c5327
Gordon <z5310742@ad.unsw.edu.au>
2023-06-26 19:54:38 +1000

    Added styling to staff menu

--- orderistic/src/components/StaffMenuCard.js
+++ orderistic/src/components/StaffMenuCard.js
@@ -4,36 +4,49 @@ import RemoveModal from './RemoveModal';
 import UpdateModal from './UpdateModal';
 import React from 'react';
 
+
 function StaffMenuCard({ element, reloadMenu }) {
-  const imgStyle = {
-    maxWidth: "260px",
-    maxHeight: "260px",
-  }
-  const cardStyle = {
-    flexDirection: "row",
-    width: "auto",
-    height: "auto",
-    maxWidth: "650px",
-    maxHeight: "260px",
-  }
   const [showRemove, setShowRemove] = React.useState(false);
   const [showUpdate, setShowUpdate] = React.useState(false);
+  const imgStyle = {
+    // maxWidth: "260px",
+    // maxHeight: "260px",
+    width: "100%",
+    objectFit: "cover",
+    height: "200px"
+  }
+  const cardStyle = {
+    display: "inline-block",
+    // flexDirection: "row",
+    height: "100%",
+    width: "100%",
+    maxHeight: "500px",
+  }
   const closeRemForm = () => setShowRemove(false);
   const showRemForm = () => setShowRemove(true);
   const closeUpdateForm = () => setShowUpdate(false);
   const showUpdateForm = () => setShowUpdate(true);
 
-  return (
+  return (  
     <>
       <Card style={cardStyle}>  
+        {/* {element.image 
+          ? <Card.Img variant="top" src={element.image} style={imgStyle}/>
+          : <></>
+        } */}
         <Card.Img variant="top" src={element.image} style={imgStyle}/>
-        <Card.Body>
-          <Card.Title>{element.name}</Card.Title>
-          <Card.Text>
+        <Card.Body style={{position: "relative", maxHeight: "200px", height: "200px"}}>
+          <Card.Title>
+            {element.name}
+          </Card.Title>
+          <Card.Text style={{ fontSize: "14px"}}>
             {element.description}
           </Card.Text>
-          <Button variant="primary" onClick={showUpdateForm}>Update Item</Button>
-          <Button variant="danger" onClick={showRemForm}>Remove Item</Button> 
+          <Card.Text style={{ position: "absolute", bottom: "5px" }}>
+            ${parseFloat(element.price).toFixed(2)}
+          </Card.Text>
+          <Button variant="outline-danger" onClick={showRemForm} style={{ position: "absolute", bottom: "15px", right: "15px"}}>Remove</Button> 
+          <Button variant="light" onClick={showUpdateForm} style={{position: "absolute", top: "10px", right: "10px"}}>✏️</Button>
         </Card.Body>
       </Card>
       <RemoveModal show={showRemove} closeForm={closeRemForm} id={element["id"]} reloadMenu={reloadMenu}/>
