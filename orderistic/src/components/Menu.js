99d47d1d75a04cbe170e9c6ad4a581eb82155e2d
Thomas Chen <thomaschen333@gmail.com>
2023-06-26 23:57:18 +1000

    Fixed variable returnData to returnFoodData

--- orderistic/src/components/Menu.js
+++ orderistic/src/components/Menu.js
@@ -1,52 +1,55 @@
 import React, { useState } from "react";
-import MenuCard from './MenuCard';
+import MenuCard from "./MenuCard";
 import SearchBar from "./SearchBar";
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import { returnData } from "../api/MenuApi";
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import { returnFoodData } from "../api/MenuApi";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 import MenuNav from "./MenuNav";
 function Menu() {
   const [searchString, setSearchString] = useState("");
   const [menu, setMenu] = React.useState([]);
   function reloadMenu() {
-    returnData()
-      .then((data) => {
-        setMenu(data);
-      })
+    returnFoodData().then((data) => {
+      setMenu(data);
+    });
   }
   React.useEffect(() => {
     reloadMenu();
-  }, [])
+  }, []);
   const search = (string) => {
     setSearchString(string);
-  }
+  };
   return (
     <>
-    <MenuNav />
-    <IconButton
-		size="large"
-		edge="start"
-		color="inherit"
-		aria-label="menu"
-		sx={{ mr: 2 }}
-		>
-		<MenuIcon />
-    </IconButton>
-    <SearchBar onSearch={search} style={{ flex:"display" }} />
-    <Row xs={1} md={2} lg={3} className="g-4" style={{ margin: "40px 40px 40px 40px"}}>
-      {menu.map((element, index) => (
-        <Col key={index} >
-          <MenuCard element={element}/>
-        </Col>
-      ))}
-    </Row>
-    
+      <MenuNav />
+      <IconButton
+        size="large"
+        edge="start"
+        color="inherit"
+        aria-label="menu"
+        sx={{ mr: 2 }}
+      >
+        <MenuIcon />
+      </IconButton>
+      <SearchBar onSearch={search} style={{ flex: "display" }} />
+      <Row
+        xs={1}
+        md={2}
+        lg={3}
+        className="g-4"
+        style={{ margin: "40px 40px 40px 40px" }}
+      >
+        {menu.map((element, index) => (
+          <Col key={index}>
+            <MenuCard element={element} />
+          </Col>
+        ))}
+      </Row>
     </>
-
-  )
+  );
 }
 
 export default Menu;
--- orderistic/src/components/StaffMenu.js
+++ orderistic/src/components/StaffMenu.js
@@ -1,43 +1,49 @@
-import StaffNav from './StaffNav';
-import StaffMenuCard from './StaffMenuCard';
-import Button from 'react-bootstrap/Button';
+import StaffNav from "./StaffNav";
+import StaffMenuCard from "./StaffMenuCard";
+import Button from "react-bootstrap/Button";
 import React from "react";
 import AddModal from "./AddModal";
-import { returnData } from "../api/MenuApi";
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
+import { returnFoodData } from "../api/MenuApi";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 function StaffMenu() {
   const [show, setShow] = React.useState(false);
   const [menu, setMenu] = React.useState([]);
   React.useEffect(() => {
-    returnData()
-      .then((data) => {
-        setMenu(data);
-      })
-  }, [])
+    returnFoodData().then((data) => {
+      setMenu(data);
+    });
+  }, []);
   const closeForm = () => setShow(false);
   const showForm = () => setShow(true);
   function reloadMenu() {
-    returnData()
-      .then((data) => {
-        setMenu(data);
-      })
+    returnFoodData().then((data) => {
+      setMenu(data);
+    });
   }
   return (
     <>
       <StaffNav />
-      <AddModal show={show} closeForm={closeForm} reloadMenu={reloadMenu}/>
-      <Row xs={1} md={2} lg={3} className="g-4" style={{ margin: "40px 40px 40px 40px"}}>
+      <AddModal show={show} closeForm={closeForm} reloadMenu={reloadMenu} />
+      <Row
+        xs={1}
+        md={2}
+        lg={3}
+        className="g-4"
+        style={{ margin: "40px 40px 40px 40px" }}
+      >
         {menu.map((element, index) => (
-          <Col key={index} >
-            <StaffMenuCard element={element} reloadMenu={reloadMenu}/>
+          <Col key={index}>
+            <StaffMenuCard element={element} reloadMenu={reloadMenu} />
           </Col>
         ))}
       </Row>
-      <Button variant="primary" onClick={showForm}>Add Item</Button>
-    </>    
-  )
+      <Button variant="primary" onClick={showForm}>
+        Add Item
+      </Button>
+    </>
+  );
 }
 
 export default StaffMenu;
