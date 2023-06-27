b7b56083273165c6723df32c977e2a11bd5db8ba
JonathanQiu <jonathan.qiu2001@outlook.com>
2023-06-27 19:20:54 +1000

    Finished table and cart implementation

--- orderistic/src/components/TestApi.js
+++ orderistic/src/components/TestApi.js
@@ -3,7 +3,8 @@ import { Card, Button, Alert } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 import { AddItem, AddItems, ViewMenu, RemoveItem, UpdateItem, returnFoodData } from "../api/MenuApi";
-import { returnOrderData, viewOrder, addOrder, removeOrder } from "../api/OrderApi"
+import { returnOrderData, viewOrder, addOrder, removeOrder } from "../api/OrderApi";
+import { addTable, removeTable, viewTables, addToCart, removeFromCart, viewCart } from "../api/TableApi";
 
 export default function TestApi() {
   const [error, setError] = useState("");
@@ -51,6 +52,30 @@ export default function TestApi() {
     returnOrderData();
   }
 
+  async function handleAddTable() {
+    addTable();
+  }
+
+  async function handleRemoveTable() {
+    removeTable();
+  }
+
+  async function handleViewTables() {
+    viewTables();
+  }
+
+  async function handleAddToCart() {
+    addToCart(1, "WTAqMRJBQyK2tDXJoFqf");
+  }
+
+  async function handleRemoveFromCart() {
+    removeFromCart(1, "WTAqMRJBQyK2tDXJoFqf");
+  }
+
+  async function handleViewCart() {
+    viewCart(1);
+  }
+
   return (
     <>
       <Card style={{ width: "75vw", maxWidth: "600px" }}>
@@ -77,6 +102,24 @@ export default function TestApi() {
           </Button>
           <Button className="btn btn-primary w-100 mt-3" onClick={handleOrderData}>
             View Order Data
+          </Button>
+          <Button className="btn btn-primary w-100 mt-3" onClick={handleAddTable}>
+            Add Table
+          </Button>
+          <Button className="btn btn-primary w-100 mt-3" onClick={handleRemoveTable}>
+            Remove Table
+          </Button>
+          <Button className="btn btn-primary w-100 mt-3" onClick={handleViewTables}>
+            View Tables
+          </Button>
+          <Button className="btn btn-primary w-100 mt-3" onClick={handleAddToCart}>
+            Add to Cart
+          </Button>
+          <Button className="btn btn-primary w-100 mt-3" onClick={handleRemoveFromCart}>
+            Remove from Cart
+          </Button>
+          <Button className="btn btn-primary w-100 mt-3" onClick={handleViewCart}>
+            View Cart
           </Button>
           <Link to="/staff-dashboard" className="btn btn-danger w-100 mt-3">
             Go Back
