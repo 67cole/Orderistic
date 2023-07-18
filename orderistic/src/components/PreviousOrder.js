32ff3c040e3f96b5746efb9a3169f8a4d0e31932
Thomas Chen <thomaschen333@gmail.com>
2023-07-18 17:39:01 +1000

    Boiler plate code for previous orders

--- orderistic/src/components/PreviousOrder.js
+++ orderistic/src/components/PreviousOrder.js
@@ -3,8 +3,13 @@ import { Card, Button, Alert } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 import MenuNav from "./MenuNav";
+import { returnOrdersForTable } from "../api/TableApi";
 
 function PreviousOrder() {
+  const { tableNumber } = useAuth();
+
+  console.log(tableNumber);
+  returnOrdersForTable(tableNumber).then((data) => console.log(data));
 
   return (
     <>
