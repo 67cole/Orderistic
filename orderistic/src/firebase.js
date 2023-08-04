b81c0d5bc06a21bce97847b4ef77560bc95f523a
Thomas Chen <thomaschen333@gmail.com>
2023-08-04 16:22:08 +1000

    Added logging to our testing page

--- orderistic/src/firebase.js
+++ orderistic/src/firebase.js
@@ -2,6 +2,8 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import { getFirestore } from "firebase/firestore";
 
+// In the real world these keys would be hidden as environment variables
+// for ease of marking we simply provided them here, acknowledging this as a security risk
 const app = firebase.initializeApp({
   apiKey: "AIzaSyAlIvEaXWA4eyvxC49UtMkVIw5gXohZckM",
   authDomain: "comp3900-orderistic.firebaseapp.com",
