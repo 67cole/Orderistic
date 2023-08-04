91b2be97013b77017f971936cb8361d654f1e280
Thomas Chen <thomaschen333@gmail.com>
2023-08-04 20:18:50 +1000

    Removing comments

--- orderistic/src/api/ReviewApi.js
+++ orderistic/src/api/ReviewApi.js
@@ -7,7 +7,7 @@ import {
   where,
   doc,
   deleteDoc,
-  setDoc
+  setDoc,
 } from "firebase/firestore";
 
 // Reviews Section
@@ -19,57 +19,55 @@ import {
 
 // Given the food ID, return the reviews
 export async function returnDishReview(itemID) {
-    const q = query(collection(db, "reviews"), where("food_id", "==", itemID));
-    const querySnapshot = await getDocs(q);
-    const reviewList = [];
-    querySnapshot.forEach((doc) => {
-        reviewList.push(doc.data());
-    });
-    return reviewList;
-  }
-  
+  const q = query(collection(db, "reviews"), where("food_id", "==", itemID));
+  const querySnapshot = await getDocs(q);
+  const reviewList = [];
+  querySnapshot.forEach((doc) => {
+    reviewList.push(doc.data());
+  });
+  return reviewList;
+}
+
 // Given the user ID, return the reviews
 export async function returnUserReview(userID) {
-    const q = query(collection(db, "reviews"), where("user_id", "==", userID));
-    const querySnapshot = await getDocs(q);
-    const reviewList = [];
-    querySnapshot.forEach((doc) => {
-        let review = doc.data();
-        review.review_id = doc.id;
-        reviewList.push(review);
-    });
-    return reviewList;
-  }
+  const q = query(collection(db, "reviews"), where("user_id", "==", userID));
+  const querySnapshot = await getDocs(q);
+  const reviewList = [];
+  querySnapshot.forEach((doc) => {
+    let review = doc.data();
+    review.review_id = doc.id;
+    reviewList.push(review);
+  });
+  return reviewList;
+}
 
 // Given the food ID, add the review to the reviews collection
 export async function addReview(review) {
-    const res = await addDoc(collection(db, 'reviews'), review);
-    return res.id;
+  const res = await addDoc(collection(db, "reviews"), review);
+  return res.id;
 }
-
-// Add a remove function later!
 
 // Grabbing all review information for front-end purposes
 export async function returnReviewData() {
-    const docRef = await getDocs(collection(db, "reviews"));
-    const reviews = {};
+  const docRef = await getDocs(collection(db, "reviews"));
+  const reviews = {};
 
-    docRef.forEach((doc) => {
-        // object holder so I can alter
-        let reviewInfo = doc.data();
-        reviews[doc.id] = reviewInfo;
-    });
-    return reviews;
+  docRef.forEach((doc) => {
+    // object holder so I can alter
+    let reviewInfo = doc.data();
+    reviews[doc.id] = reviewInfo;
+  });
+  return reviews;
 }
 
 //Allows staff to remove reviews
 export async function removeReview(reviewID) {
-    await deleteDoc(doc(db, 'reviews', reviewID));
-  }
+  await deleteDoc(doc(db, "reviews", reviewID));
+}
 
 //Allows customers to edit reviews on the menu HIMMY-21
 export async function updateReview(reviewID, newReview) {
-    await setDoc(doc(db, 'reviews', reviewID), newReview);
+  await setDoc(doc(db, "reviews", reviewID), newReview);
 }
 
 // Given the food ID, return the dish rating
@@ -79,8 +77,8 @@ export async function returnDishRating(itemID) {
   let totalRating = 0;
   let divisor = 0;
   querySnapshot.forEach((doc) => {
-      totalRating += doc.data()["rating"];
-      divisor += 1;
+    totalRating += doc.data()["rating"];
+    divisor += 1;
   });
-  return totalRating/divisor;
+  return totalRating / divisor;
 }
