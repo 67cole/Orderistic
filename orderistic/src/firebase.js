59b5acbdb4ad1964b5f56bc4b8d79ded3d736a27
Thomas <65510993+tc3149@users.noreply.github.com>
2023-09-09 15:12:26 +1000

    Update firebase.js

--- orderistic/src/firebase.js
+++ orderistic/src/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 // In the real world these keys would be hidden as environment variables
 // for ease of marking we simply provided them here, acknowledging this as a security risk
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyAlIvEaXWA4eyvxC49UtMkVIw5gXohZckM",
-  authDomain: "comp3900-orderistic.firebaseapp.com",
-  projectId: "comp3900-orderistic",
-  storageBucket: "comp3900-orderistic.appspot.com",
-  messagingSenderId: "1028996594361",
-  appId: "1:1028996594361:web:2aea9f57fa1569be8b7968",
+  apiKey: APIKEY,
+  authDomain: AUTHDOMAIN,
+  projectId: PROJECTID,
+  storageBucket: STORAGEBUCKET,
+  messagingSenderId: MESSAGINGSENDERID,
+  appId: APPID,
 });
 
 export const auth = app.auth();
